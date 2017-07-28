05da628d46366b905e97dd5d5372f9947ce56443
Zopanix <zopanix@gmail.com>
2017-07-28 16:23:52 -0400

    Fixed entrypoint

--- resources/entrypoint.sh
+++ resources/entrypoint.sh
@@ -2,7 +2,7 @@
 set -e -x
 
 # Fetching host nameserver
-if [ "$RESOLVER" -eq "host" ]; then
+if [ "$RESOLVER" = 'host' ]; then
   export NAMESERVER=$(cat /etc/resolv.conf | grep nameserver | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b")
 fi
 
